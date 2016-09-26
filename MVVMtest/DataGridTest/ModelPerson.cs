8ec60b1b6d1aca031aa38cf1e7aee10a900f6299
foreverlearner <beilei.zhan@hotmail.com>
2016-09-26 16:58:39 -0400

    add DataGridTest project

---
+++ MVVMtest/DataGridTest/ModelPerson.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridTest
+{
+    public class ModelPerson : INotifyPropertyChanged
+    {
+        string _FirstName;
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set
+            {
+                if (_FirstName != value)
+                {
+                    _FirstName = value;
+                    RaisePropertyChanged("FirstName");
+                }
+            }
+        }
+
+        string _LastName;
+        public string LastName
+        {
+            get { return _LastName; }
+            set
+            {
+                if (_LastName != value)
+                {
+                    _LastName = value;
+                    RaisePropertyChanged("LastName");
+                }
+            }
+        }
+
+        int _Age;
+        public int Age
+        {
+            get { return _Age; }
+            set
+            {
+                if(_Age != value)
+                {
+                    _Age = value;
+                    RaisePropertyChanged("Age");
+                }
+            }
+        }
+
+        void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ MVVMtest/DataGridTest/ViewModelMain.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridTest
+{
+    class ViewModelMain : ViewModelBase
+    {
+        public ObservableCollection<ModelPerson> People { get; set; }
+
+        object _SelectedPerson;
+        public object SelectedPerson
+        {
+            get { return _SelectedPerson; }
+            set
+            {
+                if (_SelectedPerson != value)
+                {
+                    _SelectedPerson = value;
+                    RaisePropertyChanged("SelectedPerson");
+                }
+            }
+        }
+
+        string _TextProperty1;
+        public string TextProperty1
+        {
+            get { return _TextProperty1; }
+            set
+            {
+                if (_TextProperty1 != value)
+                {
+                    _TextProperty1 = value;
+                    RaisePropertyChanged("TextProperty1");
+                }
+            }
+        }
+
+        public RelayCommand AddUserCommand { get; set; }
+        public ViewModelMain()
+        {
+            People = new ObservableCollection<ModelPerson>
+            {
+                new ModelPerson { FirstName="Tom", LastName="Jones", Age=80 },
+                new ModelPerson { FirstName="Dick", LastName="Tracey", Age=40 },
+                new ModelPerson { FirstName="Harry", LastName="Hill", Age=60 },
+            };
+            TextProperty1 = "Type here";
+
+            AddUserCommand = new RelayCommand(AddUser);
+        }
+
+        void AddUser(object parameter)
+        {
+            if (parameter == null) return;
+            People.Add(new ModelPerson { FirstName = parameter.ToString(), LastName = parameter.ToString(), Age = DateTime.Now.Second });
+        }
+    }
+}
