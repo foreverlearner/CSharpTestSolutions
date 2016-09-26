5eada6b9fcb988dca93bbca15c26e963871c18fc
foreverlearner <beilei.zhan@hotmail.com>
2016-09-26 10:46:58 -0400

    Add GetTextboxInput project

---
+++ MVVMtest/GetTextboxInput/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GetTextboxInput
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Numbers _numbers;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _numbers = (Numbers)this.DataContext;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _numbers.NotifyPropertyChange("Answer");
+        }
+    }
+}
---
+++ MVVMtest/GetTextboxInput/Numbers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetTextboxInput
+{
+    class Numbers : INotifyPropertyChanged
+    {
+        #region Members
+        int _firstNo;
+        int _secondNo;
+        #endregion
+
+        #region Properties
+        public int FirstNo
+        {
+            get { return _firstNo; }
+            set
+            {
+                _firstNo = value;
+                NotifyPropertyChange("FirstNo");
+            }
+        }
+
+        public int SecondNo
+        {
+            get { return _secondNo; }
+            set
+            {
+                _secondNo = value;
+                NotifyPropertyChange("SecondNo");
+            }
+        }
+
+        public int Answer
+        {
+            get { return FirstNo + SecondNo; }
+        }
+        #endregion
+
+        #region NotifyPropertyChange
+        /// <summary>
+        /// Occurs when a property value changes
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raise the  <see cref="PropertyChanged"/> event.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public void NotifyPropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion    
+    }
+}
